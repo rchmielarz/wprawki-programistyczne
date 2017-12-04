077085d8d7910c7b3d38ed55e9a52ae80a0eafc0
Radoslaw Chmielarz <radoslaw@chmielarz.xyz>
2017-12-04 23:21:44 +0100

    Advent of Code 2017: day 4 part 2 added

--- advent_of_code2017/day4.py
+++ advent_of_code2017/day4.py
@@ -6,13 +6,18 @@ def is_valid_passphrase(passphrase):
     return len(passphrase_list) == len(set(passphrase_list))
 
 
-def count_valid_passphrases(input):
+def is_valid_passphrase2(passphrase):
+    passphrase_list = ["".join(sorted(word)) for word in passphrase.split()]
+    return len(passphrase_list) == len(set(passphrase_list))
+
+
+def count_valid_passphrases(input, passphrase_validator):
     sum = 0
     for line in input.splitlines():
-        if is_valid_passphrase(line):
+        if passphrase_validator(line):
             sum += 1
     return sum
 
 
 with open('day4_input.txt', 'r') as f:
-    print(count_valid_passphrases(f.read()))
+    print(count_valid_passphrases(f.read(), is_valid_passphrase2))
