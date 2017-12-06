08461d7e485a73b8585f7f5394ad6b0e6020080b
Radoslaw Chmielarz <radoslaw@chmielarz.xyz>
2017-12-06 22:36:09 +0100

    Advent of Code 2017: day 3 part 2 added

--- advent_of_code2017/day3.py
+++ advent_of_code2017/day3.py
@@ -28,6 +28,50 @@ def calculate_position(n):
     return position
 
 
+def neighbours(point):
+    return [(point[0] - 1, point[1]),
+            (point[0] - 1, point[1] - 1),
+            (point[0] - 1, point[1] + 1),
+            (point[0], point[1] - 1),
+            (point[0], point[1] + 1),
+            (point[0] + 1, point[1]),
+            (point[0] + 1, point[1] - 1),
+            (point[0] + 1, point[1] + 1)]
+
+
+def get_neighbours_values(point, grid):
+    sum = 0
+    for neighbour in neighbours(point):
+        if neighbour in grid:
+            sum += grid[(neighbour[0], neighbour[1])]
+    return sum
+
+
+def spiral_memory2(n):
+    position = [0, 0]
+    current_direction_index = 0
+    direction_repetitions = 1
+    current_repetitions = 1
+    grid = {(0, 0): 1}
+    while n >= grid[(position[0], position[1])]:
+        position[0] += directions[current_direction_index][0]
+        position[1] += directions[current_direction_index][1]
+        grid[(position[0], position[1])] = get_neighbours_values(position, grid)
+        current_repetitions -= 1
+        if current_repetitions == 0:
+            if current_direction_index == 3:
+                current_direction_index = 0
+            else:
+                current_direction_index += 1
+
+            if current_direction_index in (0, 2):
+                direction_repetitions += 1
+
+            current_repetitions = direction_repetitions
+
+    return grid[(position[0], position[1])]
+
+
 def spiral_memory(n):
     position = calculate_position(n)
     return abs(position[0]) + abs(position[1])
