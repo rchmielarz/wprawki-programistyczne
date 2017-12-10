3c1015d20a9eebea0d38db6b1aea77a089d931ad
Radoslaw Chmielarz <radoslaw@chmielarz.xyz>
2017-12-11 00:21:08 +0100

    Advent of Code 2017: day 8 part 2 added

--- advent_of_code2017/day8.py
+++ advent_of_code2017/day8.py
@@ -19,6 +19,25 @@ def parse_instructions(lines):
     return max(register.values())
 
 
+def parse_instructions2(lines):
+    max_value = 0
+    register = defaultdict(int)
+    for line in lines:
+        (command, condition) = line.split('if')
+        condition_tokens = condition.split()
+        if eval("register['" + condition_tokens[0] + "']" + ''.join(condition_tokens[1:])):
+            command_tokens = command.split()
+            if command_tokens[1] == 'inc':
+                command_tokens[1] = '+'
+            else:
+                command_tokens[1] = '-'
+            register[command_tokens[0]] = eval(
+                "register['" + command_tokens[0] + "']" + ''.join(command_tokens[1:]))
+            current_max = max(register.values())
+            if current_max > max_value:
+                max_value = current_max
+    return max_value
+
+
 with open('day8_input.txt', 'r') as f:
-    l = f.read().splitlines()
-    print(parse_instructions(l))
+    print(parse_instructions2(f.read().splitlines()))
